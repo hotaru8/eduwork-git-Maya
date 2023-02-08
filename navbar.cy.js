aca242ebe8a8d49e468dcf4e643eeccc33fb2a77
hotaru8 <124076501+hotaru8@users.noreply.github.com>
2023-02-09 01:55:37 +0700

    Add files via upload

---
+++ navbar.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('Navbar Test', () => {
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+    it('should display online banking content', () => {
+        cy.get('#onlineBankingMenu').click()
+        cy.url().should('include','online-banking.html')
+        cy.get('h1').should('be.visible')
+    })
+    it('should display feedback content', () => {
+        cy.get('#feedback').click()
+        cy.url().should('include','feedback.html')
+    });
+    it('should display homepage content', () => {
+        cy.contains('Zero Bank').click()
+        cy.get('#account_activity_link').should('contain', 'Checking Account Activity')
+    });
+        
+})
