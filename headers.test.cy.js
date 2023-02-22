303a24134f3cf8a1685ed7e572238c66d50f96f6
hotaru8 <124076501+hotaru8@users.noreply.github.com>
2023-02-23 01:34:16 +0700

    Add files via upload

---
+++ headers.test.cy.js
@@ -0,0 +1,10 @@
+describe("Validate Headers", () => {
+  it("Successfully validate content-type", () => {
+    cy.request("https://pokeapi.co/api/v2/pokemon/ditto").as("pokemon");
+    cy.get("@pokemon")
+      .its("headers")
+      .its("content-type")
+      .should("include", "application/json; charset=utf-8");
+    cy.get("@pokemon").its("body").should("abilities", "name");
+  });
+});
